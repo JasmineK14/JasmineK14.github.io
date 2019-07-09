70c0ed7baee3d88101d0e65c782dd9293f23cf21
JasmineK14 <jasmine.kinney14@gmail.com>
2019-07-09 15:38:26 -0700

    initial commmit

---
+++ ab.js
@@ -0,0 +1,11 @@
+alert ("Welcome to my page!");
+
+function bigImg(x){
+  x.style.height = "400px";
+  x.style.width = "900px";
+}
+
+function originalImg(x){
+    x.style.height = "200px";
+    x.style.width = "400px";
+}
